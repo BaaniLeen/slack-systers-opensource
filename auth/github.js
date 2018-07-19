ef5cd82db4253de42e888cc89f3dfebd704915c0
BaaniLeen <baani.jolly@gmail.com>
2018-07-19 22:36:30 +0530

    AddTnC

--- auth/github.js
+++ auth/github.js
@@ -4,35 +4,44 @@ var nodeMailer = require("nodemailer");
 var User = require('../models/User');
 
 passport.use(new GitHubStrategy({
-    clientID: "e7b10decd2ed4ef13816",
-    clientSecret: "bb073a53914d014f328de98ad9fe5a3cff366912",
+    clientID: "94c3bea3e111456764eb",
+    clientSecret: "b665c678fe607b3460b50c41fb01d801848f207a",
     callbackURL: "http://127.0.0.1:3000/auth/github/callback"
   },
-  function(accessToken, refreshToken, profile, done) {
+  function (accessToken, refreshToken, profile, done) {
     console.log(profile.emails[0].value);
     let transporter = nodeMailer.createTransport({
       host: "smtp.gmail.com",
       port: 465,
       secure: true,
       auth: {
-          user: "baani16234@iiitd.ac.in",
-          pass: ""
+        user: "baani.jolly@gmail.com",
+        pass: ""
       }
     });
     let mailOptions = {
-      from: "<baani16234@iiitd.ac.in>", // sender address
+      from: "<baani.jolly@gmail.com>", // sender address
       to: profile.emails[0].value, // list of receivers
-      subject: "Systers OSS Slack Invite", // Subject line
-      text: "Good morning!", // plain text body
-      html: "<b>Demo</b>" // html body
+      subject: "Slack Invitation", // Subject line
+      text: "Here is your link", // plain text body
+      html: "<b>Hello World</b>" // html body
     };
-    
-    transporter.sendMail(mailOptions, (error, info) => {    
+
+    transporter.sendMail(mailOptions, (error, info) => {
+
       if (!error) {
-          res.render("index");
-        }    
-      });
-    User.findOrCreate({userid: profile.id}, {name: profile.displayName, userid: profile.id, email: profile.emails[0].value}, function (err, user) {
+        return console.log(error);
+      }
+
+    });
+
+    User.findOrCreate({
+      userid: profile.id
+    }, {
+      name: profile.displayName,
+      userid: profile.id,
+      emailid: profile.emails[0].value
+    }, function (err, user) {
       return done(err, user);
     });
   }
