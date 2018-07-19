4da66fe50a1a7d34b260b386805b6b405ea94e5a
BaaniLeen <baani.jolly@gmail.com>
2018-07-19 07:31:38 +0530

    Ported Home Page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { HttpModule } from '@angular/http';
-import { FormsModule } from '@angular/forms';
-
+import { BrowserModule, Title } from '@angular/platform-browser';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpModule } from '@angular/http';
 import { CommonService } from './common.service';
+import { FormComponent } from './form/form.component';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FormComponent
   ],
   imports: [
-    BrowserModule, HttpModule, FormsModule,
+BrowserModule,
+    BrowserAnimationsModule,
+    HttpModule,
+    FormsModule,
+    AppRoutingModule,
+    NgbModule.forRoot(),
   ],
   providers: [CommonService],
   bootstrap: [AppComponent]
