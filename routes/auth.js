72fdecdd0649c59adcc1637fe321710694974ab6
BaaniLeen <baani.jolly@gmail.com>
2018-07-20 17:53:44 +0530

    Refined the UI

--- routes/auth.js
+++ routes/auth.js
@@ -17,37 +17,6 @@ router.get('/logout', function(req, res){
   res.redirect('/');
 });
 
-/* FACEBOOK ROUTER */
-router.get('/facebook',
-  passportFacebook.authenticate('facebook'));
-
-router.get('/facebook/callback',
-  passportFacebook.authenticate('facebook', { failureRedirect: '/login' }),
-  function(req, res) {
-    // Successful authentication, redirect home.
-    res.redirect('/users');
-  });
-
-/* TWITTER ROUTER */
-router.get('/twitter',
-  passportTwitter.authenticate('twitter'));
-
-router.get('/twitter/callback',
-  passportTwitter.authenticate('twitter', { failureRedirect: '/login' }),
-  function(req, res) {
-    res.redirect('/users');
-  });
-
-/* GOOGLE ROUTER */
-router.get('/google',
-  passportGoogle.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login'] }));
-
-router.get('/google/callback',
-  passportGoogle.authenticate('google', { failureRedirect: '/login' }),
-  function(req, res) {
-    res.redirect('/users');
-  });
-
 /* GITHUB ROUTER */
 router.get('/github',
   passportGitHub.authenticate('github', { scope: [ 'user:email' ] }));
